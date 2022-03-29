d263cef134c5003c487c5cdbbd35a1cedd6d9de5
George Baev <geobaev@gmail.com>
2022-03-29 08:28:21 +0300

    ASUIS-280: Launch the Web Directory component.

---
+++ js/webspark_webdir.js
@@ -0,0 +1,40 @@
+(function ($, Drupal, drupalSettings, once) {
+
+  Drupal.behaviors.webSparkWebDir = {
+    attach: function (context, settings) {
+      var componentLoaded = typeof webdirUI !== "undefined" && typeof webdirUI.initSearchPage !== "undefined";
+
+      if (!componentLoaded) {
+        return;
+      }
+
+      const elements = once('webSparkWebDir', '.webdir-container', context);
+
+      elements.forEach((value, index) => {
+        props = {
+          searchType: value.dataset.searchType,
+          searchURL: value.dataset.searchUrl,
+          peopleSearch: value.dataset.peopleSearch,
+          ids: value.dataset.asuriteIds,
+          deptIds: value.dataset.deptIds,
+          filters: {
+            employee: value.dataset.filterEmployee,
+            expertise: value.dataset.filterExpertise,
+            title: value.dataset.filterTitle
+          },
+          display: {
+            defaultSort: value.dataset.defaultSort,
+            usePager: value.dataset.usePager,
+            profilesPerPage: value.dataset.profilesPerPage,
+            doNotDisplayProfiles: value.dataset.doNotDisplayProfiles,
+          }
+        };
+
+        webdirUI.initWebDirectory({
+          targetSelector: "#" + value.id,
+          props: props,
+        });
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings, once);
