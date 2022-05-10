c8f074cf586c8e1599b288c177c24e44cc5dfcd8
George Baev <geobaev@gmail.com>
2022-05-10 11:34:29 +0300

    ASUIS-467: Fix names and remove duplicates.

--- js/add-field.js
+++ js/add-field.js
@@ -5,10 +5,10 @@
     attach: function (context, settings) {
       // Check if there are directory type fields.
       if ($(context).find('.asurite-add').length) {
-        $('.asurite-add').each(function(index) {
+        $('.asurite-add').each(function (index) {
           // Convert and check the default values.
           initialize_tree();
-          $(".directory-tree").on('change', function() {
+          $(".directory-tree").on('change', function () {
             // Update the tree.
             update_tree();
           });
@@ -33,93 +33,110 @@
     }
   };
 
-// Convert the json from the asuriteid to be compatible with the jstree.
-function convert_asurite_to_tree(data, departments, existing_values) {
-  var result = [];
-  var temp = {};
-  $(data).each(function (i, element) {
-    $(element.deptids.raw).each(function (j, deptid) {
-      var new_element = {};
-      if (departments.includes(deptid) &&
+  // Convert the json from the asuriteid to be compatible with the jstree.
+  function convert_asurite_to_tree(data, departments, existing_values) {
+    var result = [];
+    var temp = {};
+    $(data).each(function (i, element) {
+      $(element.deptids.raw).each(function (j, deptid) {
+        var new_element = {};
+        if (departments.includes(deptid) &&
           !existing_values.includes(element.asurite_id.raw + ':' + deptid)) {
 
-        var department_data = getDepartmentData(element, deptid);
+          var department_data = getDepartmentData(element, deptid);
 
-        new_element.id = element.asurite_id.raw + ':' + deptid;
-        // Remove maybe.
-        new_element.sort = element.last_name.raw;
-        new_element.text = element.display_name.raw + ', ' + element.asurite_id.raw +
-                ', ' + department_data['name']+ ', ' + department_data['title'];
-        new_element.type = "person";
-        if (!temp.hasOwnProperty(deptid)) {
-          temp[deptid] = [];
+          new_element.id = element.asurite_id.raw + ':' + deptid;
+          // Remove maybe.
+          new_element.sort = element.last_name.raw;
+          new_element.text = element.display_name.raw + ', ' + element.asurite_id.raw +
+            ', ' + department_data['name'] + ', ' + department_data['title'];
+          new_element.type = "person";
+          if (!temp.hasOwnProperty(deptid)) {
+            temp[deptid] = [];
+          }
+          temp[deptid].push(new_element);
         }
-        temp[deptid].push(new_element);
-      }
+      });
     });
-  });
-  // Collapse and sort the temp into result.
-  Object.keys(temp).forEach(deptID => {
-    var sorted = temp[deptID];
-    sorted.sort(function(a, b){
-      return (a["sort"] < b["sort"]) ? -1 : (a["sort"] > b["sort"]) ? 1 : 0;
-    });
-    result = result.concat(sorted);
-  });
 
-  return result;
-}
+    // Filter out duplicate keys, collapse and sort the temp into result.
+    Object.keys(temp).forEach(deptID => {
+      var sorted = temp[deptID];
 
-// @TODO Mark is going to build similar functionality. Replace this when that is done.
-function getDepartmentData($data, $deptId) {
-  var result = [];
-  result['name'] = 'Department Name' + $deptId;
-  result['title'] = 'Department Title' + $deptId;
+      const uniqueIds = new Set();
 
-  return result;
-}
+      const unique = sorted.filter(element => {
+        const isDuplicate = uniqueIds.has(element.id);
 
-// Prepare parameters for the asurite id solr call.
-function createCallParams(ids) {
-  return "?dept_id[]=" + ids.join('&dept_id[]=') + "&size=1000";
-}
+        uniqueIds.add(element.id);
 
-// Create the asurite id checkboxes.
-function initialize_tree() {
-  var departments = $(".directory-tree").val().split(',');
-  var query = createCallParams(departments);
-  $.getJSON("/endpoint/people-in-department"+query, function(json) {
-    var existing_values = $('.asurite-tree').val().split(',');
-    converted_json = convert_asurite_to_tree(json.results, departments, existing_values);
-    $('#asurite-add-options') // listen for event
-    .jstree({
-      'core' : {
-        'data' : converted_json,
-        'themes' : { dots: false }
-      },
-      types: {
-        "person": {
-          "icon" : "fa fa-user"
-        },
-        "default" : {
+        if (!isDuplicate) {
+          return true;
         }
-      },
-      "plugins" : [ "types" ]
+
+        return false;
+      });
+
+      unique.sort(function (a, b) {
+        return (a["sort"] < b["sort"]) ? -1 : (a["sort"] > b["sort"]) ? 1 : 0;
+      });
+
+      result = result.concat(unique);
     });
-  });
-}
-// Create the asurite id checkboxes.
-function update_tree() {
-  var departments = $( ".directory-tree" ).val().split(',');
-//  var departments = ['1661','1374'];
-  var query = createCallParams(departments);
-  $.getJSON("/endpoint/people-in-department"+query, function(json) {
-    // Get existing data.
-    var existing_values = $('.asurite-tree').val().split(',');
-    converted_json = convert_asurite_to_tree(json.results, departments, existing_values);
-    $('#asurite-add-options').jstree(true).settings.core.data = converted_json;
-    $('#asurite-add-options').jstree(true).refresh();
-  });
-}
+
+    return result;
+  }
+
+  function getDepartmentData($data, $deptId) {
+    var result = [];
+    result['name'] = $data.departments.raw[0];
+    result['title'] = $data.titles.raw[0];
+
+    return result;
+  }
+
+  // Prepare parameters for the asurite id solr call.
+  function createCallParams(ids) {
+    return "?dept_id[]=" + ids.join('&dept_id[]=') + "&size=1000";
+  }
+
+  // Create the asurite id checkboxes.
+  function initialize_tree() {
+    var departments = $(".directory-tree").val().split(',');
+    var query = createCallParams(departments);
+    $.getJSON("/endpoint/people-in-department" + query, function (json) {
+      var existing_values = $('.asurite-tree').val().split(',');
+      converted_json = convert_asurite_to_tree(json.results, departments, existing_values);
+      $('#asurite-add-options') // listen for event
+        .jstree({
+          'core': {
+            'data': converted_json,
+            'themes': { dots: false }
+          },
+          types: {
+            "person": {
+              "icon": "fa fa-user"
+            },
+            "default": {
+            }
+          },
+          "plugins": ["types"]
+        });
+    });
+  }
+
+  // Create the asurite id checkboxes.
+  function update_tree() {
+    var departments = $(".directory-tree").val().split(',');
+    //  var departments = ['1661','1374'];
+    var query = createCallParams(departments);
+    $.getJSON("/endpoint/people-in-department" + query, function (json) {
+      // Get existing data.
+      var existing_values = $('.asurite-tree').val().split(',');
+      converted_json = convert_asurite_to_tree(json.results, departments, existing_values);
+      $('#asurite-add-options').jstree(true).settings.core.data = converted_json;
+      $('#asurite-add-options').jstree(true).refresh();
+    });
+  }
 
 })(jQuery, Drupal, drupalSettings);
